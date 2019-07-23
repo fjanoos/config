ac381af1528d0b4f05118bc8f3629ab745ba911f
fjanoos <firdaus@ieee.org>
2019-07-23 11:20:22 +0000

    Checkpoint.

--- configure.sh
+++ configure.sh
@@ -4,3 +4,5 @@ ln -b -s ~/config/.tmux.conf -t ~/
 ~/config/configure_git.sh
 ln -b -s ~/config/.git-prompt.sh -t ~/
 ln -b -s ~/config/.gitignore -t ~/
+ln -b -s ~/config/.jupyter -t ~/
+ln -b -s ~/config/.ipython -t ~/
