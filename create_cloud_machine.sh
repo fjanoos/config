aac2b4c32f85e6ddb5ca0b2d4bb571c035f3a9e9
fjanoos <firdaus@ieee.org>
2020-04-13 20:31:11 -0400

    Checkpoint Mon Apr 13 20:31:11 EDT 2020

--- create_cloud_machine.sh
+++ create_cloud_machine.sh
@@ -82,11 +82,11 @@ chmod au+x Anaconda3-2020.02-Linux-x86_64.sh
 ### Install conda packages into main enviroment ###
 conda deactivate
 conda install numpy numba tensorboard arrow joblib pandas matplotlib jupyter memory_profiler pyarrow xarray statsmodels scipy scikit-learn dask tqdm sqlalchemy
-pip install itables p3nvml pygments cvxpy psycopg2 cupy
+pip install itables pygments cvxpy psycopg2 cupy  jupyterlab
 # install torch as required
 conda install pytorch cudatoolkit=10.1 -c pytorch 
 # command line csv viewier ( /> vd ... )
-conda install -c conda-forge jupyter_contrib_nbextensions visidata
+conda install -c conda-forge jupyter_contrib_nbextensions visidata 
 jupyter contrib nbextension install --user
 jupyter nbextension enable codefolding/main
 jupyter nbextension enable toggle_all_line_numbers/main
